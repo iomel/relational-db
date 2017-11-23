feed1ae6cd5df84cd4f9f088bc1e93b657902ce7
iomel <igor.omel@gmail.com>
2017-11-23 14:40:03 +0100

    lesson 2 and 3 SQL script added

--- src/lesson2/Demo.java
+++ src/lesson2/Demo.java
@@ -1,11 +1,15 @@
 package lesson2;
 
+import java.util.Date;
+
 public class Demo {
     public static void main(String[] args) {
         // java
         // create class
         // create object with values
         // use object
+
+        Order order = new Order(1101, "TOOL2122", 140, new Date(), new Date());
 
         // db
         // create table with fields/columns
