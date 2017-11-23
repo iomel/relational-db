0b4a61935d17b4ae2b537fec8ba2d0f9e902c861
iomel <igor.omel@gmail.com>
2017-11-23 09:11:35 +0100

    lesson 2 db create order table

---
+++ src/lesson2/Demo.java
@@ -0,0 +1,16 @@
+package lesson2;
+
+public class Demo {
+    public static void main(String[] args) {
+        // java
+        // create class
+        // create object with values
+        // use object
+
+        // db
+        // create table with fields/columns
+        // inert date to the table
+
+
+    }
+}
